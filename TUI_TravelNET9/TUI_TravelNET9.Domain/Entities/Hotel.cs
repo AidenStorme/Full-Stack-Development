e6d34dc1999f9b04553b056c77f881664be462af
“AidenStorme” <aiden.storme@student.vives.be>
2025-11-19 16:17:59 +0100

    Initialize TUI_TravelNET9 project structure with domain, repository, and service layers; add controllers, views, and necessary configurations

--- TUI_TravelNET9/TUI_TravelNET9.Domain/Entities/Hotel.cs
+++ TUI_TravelNET9/TUI_TravelNET9.Domain/Entities/Hotel.cs
@@ -2,5 +2,20 @@ namespace TUI_TravelNET9.Domain.Entities;
 
 public class Hotel
 {
-    
+    public enum CountryType
+    {
+        Coast,
+        Wallonia
+    }
+    public class Vacation
+    {
+        public int Id { get; set; }
+        public string? NameHotel { get; set; }
+        public string? City { get; set; }
+        public int Stars { get; set; }
+        public double Score { get; set; }
+        public string? Benefit { get; set; }
+        public string? Photo { get; set; }
+        public CountryType Country { get; set; }
+    }
 }
