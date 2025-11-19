e6c999e4023adcad6ea694c4fadb370b49ddbb07
“AidenStorme” <aiden.storme@student.vives.be>
2025-11-19 10:34:07 +0100

    Initial commit: add ASP.NET Core MVC project structure with controllers, views, and models

---
+++ RichPanel9/RichPanel9/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RichPanel9.ViewModels;
+
+namespace RichPanel9.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(ProductVM product)
+        {
+            return View("Richoutput", product);
+        }
+    }
+}
---
+++ RichPanel9/RichPanel9/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RichPanel9.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
